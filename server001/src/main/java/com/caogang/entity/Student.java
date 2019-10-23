560866bae8fba45dee9ea77272d6e28cbcb3ed0f
xiaogang0827 <18242360858@163.com>
2019-10-23 17:08:12 +0800

    branch001

--- server001/src/main/java/com/caogang/entity/Student.java
+++ server001/src/main/java/com/caogang/entity/Student.java
@@ -1,6 +1,8 @@
 package com.caogang.entity;
 
 import java.math.BigDecimal;
+import java.util.Date;
+
 import lombok.Data;
 
 /** 
@@ -15,6 +17,7 @@ public class Student {
 	private String name;
 	private Integer age;
 	private BigDecimal salary;
+	private Date birthday;
 	private String sex;
 	
 }
